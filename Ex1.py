86e2e36717cd82c06e0780bfce7f58ec126a2792
Zhu Gewen <21459533@life.hkbu.edu.hk>
2022-02-23 19:05:01 +0800

    finished lab3

--- Ex1.py
+++
@@ -1,7 +0,0 @@
-# Find the all factors of x using a loop and the operator % 
-# % means find remainder, for example 10 % 2 = 0; 10% 3 = 1
-if __name__ == '__main__':
-    x = 52633
-    for i in range(x+1):
-        # your code here
-        if( not i==0 and x%i==0): print(i)
--- Ex2.py
+++
@@ -1,8 +0,0 @@
-# Write a function that prints all factors of the given parameter x
-def print_factor(x):
-  # your code here
-  for i in range(x+1):
-      if(not i==0 and x%i==0): print(i)
-
-if __name__ == '__main__':
-    print_factor(52633)
