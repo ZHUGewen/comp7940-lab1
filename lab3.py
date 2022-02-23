8b75bb04a98c15dcb03bda1943c637084a170770
Zhu Gewen <21459533@life.hkbu.edu.hk>
2022-02-23 22:50:42 +0800

    finished lab4 write up

--- lab3.py
+++ lab3.py
@@ -19,6 +19,7 @@ def main():
     # Load your token and create an Updater for your Bot
     config = configparser.ConfigParser()
     config.read('config.ini')
+    print(config['TELEGRAM']['ACCESS_TOKEN'])
     updater = Updater(token=(config['TELEGRAM']['ACCESS_TOKEN']), use_context=True)
     dispatcher = updater.dispatcher
     # You can set this logging module, so you will know when and why things do not work as expected
